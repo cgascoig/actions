b89dd8c40d3d55ebeb765fb3ac7722d7fa3cd304
Chris Gascoigne <cgascoig@cisco.com>
2019-01-08 10:33:15 +1100

    send markdown formatted message

--- webexteams/webexteams.py
+++ webexteams/webexteams.py
@@ -13,14 +13,27 @@ def main():
     room_name = ' '.join(args.room)
     user_message = ' '.join(args.message)
 
-    message = f"Event: {os.environ.get('GITHUB_EVENT_NAME')} Repository: {os.environ.get('GITHUB_REPOSITORY')} Initiated by: {os.environ.get('GITHUB_ACTOR')} Workflow: {os.environ.get('GITHUB_WORKFLOW')} Message: {user_message}"
+    message = f"Event: {os.environ.get('GITHUB_EVENT_NAME')} Repository: {os.environ.get('GITHUB_REPOSITORY')} Commit: {os.environ.get('GITHUB_SHA')} Initiated by: {os.environ.get('GITHUB_ACTOR')} Workflow: {os.environ.get('GITHUB_WORKFLOW')} Message: {user_message}"
+    markdown_message = f"""
+    **Event:** {os.environ.get('GITHUB_EVENT_NAME')} 
+    
+    **Repository:** {os.environ.get('GITHUB_REPOSITORY')} 
+    
+    **Commit:** {os.environ.get('GITHUB_SHA')} 
+    
+    **Initiated by:** {os.environ.get('GITHUB_ACTOR')} 
+    
+    **Workflow:** {os.environ.get('GITHUB_WORKFLOW')} 
+    
+    **Message:** {user_message}
+    """
 
     print(f"Attempting to send message '{message}' to room '{room_name}'")
     rooms = api.rooms.list()
     for room in rooms:
         if room.title == room_name:
             print(f"Found room {room.id} with title '{room.title}'")
-            api.messages.create(room.id, text=message)
+            api.messages.create(room.id, text=message, markdown=markdown_message)
             return
 
     print("Room not found, no message sent")
