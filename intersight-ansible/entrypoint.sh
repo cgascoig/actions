0227926ebe9b0ececd4de27fa05e0759cee22bf7
Chris Gascoigne <cgascoig@cisco.com>
2019-01-17 18:07:37 +1100

    shrink docker image by using alpine base and multi-stage build

--- intersight-ansible/entrypoint.sh
+++ intersight-ansible/entrypoint.sh
@@ -3,7 +3,7 @@
 # echo "${INTERSIGHT_API_KEY_ID}"> /intersight_api_key_id
 echo "${INTERSIGHT_API_PRIVATE_KEY}" > /intersight_api_private_key
 mkdir /runner/env
-echo "---\nintersight_api_key_id: ${INTERSIGHT_API_KEY_ID}" > /runner/env/extravars
+printf "---\nintersight_api_key_id: ${INTERSIGHT_API_KEY_ID}" > /runner/env/extravars
 
 PLAYBOOK="${PLAYBOOK:-main.yml}"
 
