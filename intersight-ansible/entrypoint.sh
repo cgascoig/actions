50cce67d152aaa4600e97d562bb43dc67feaf80c
Chris Gascoigne <cgascoig@cisco.com>
2019-02-04 16:23:21 +1100

    enable ANSIBLE_JINJA2_NATIVE

--- intersight-ansible/entrypoint.sh
+++ intersight-ansible/entrypoint.sh
@@ -4,6 +4,7 @@
 echo "${INTERSIGHT_API_PRIVATE_KEY}" > /intersight_api_private_key
 mkdir /runner/env
 printf "---\nintersight_api_key_id: ${INTERSIGHT_API_KEY_ID}" > /runner/env/extravars
+printf "---\nANSIBLE_JINJA2_NATIVE: true" > /runner/env/envvars
 
 PLAYBOOK="${PLAYBOOK:-main.yml}"
 
